578cf62350d8f5d45db08bbfbf5f928afc84eabb
Alvaro <alvaro.estebanez@braintec-group.com>
2017-11-07 16:23:47 +0100

    FIX

--- jasper_server/report/report_soap.py
+++ jasper_server/report/report_soap.py
@@ -366,7 +366,7 @@ class Report(object):
             d_par.update({
                 'company_name': cny.name,
                 'company_logo': cny.name.encode('ascii',
-                                                'ignore').replace(' ', '_'),
+                                                'ignore').replace(' ', '_') if cny.name else '',
                 'company_header1': cny.rml_header1 or '',
                 'company_footer1': cny.rml_footer or '',
                 'company_footer2': '',
